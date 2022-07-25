20a9d60d9a36c94fd6b3f9c93589b4647937fce8
Franz J Schreiber <mail@franz-schreiber.eu>
2022-07-25 16:56:04 +0200

    Fourier series, measurement function Z_n, etc.

--- numerics/utils.jl
+++ numerics/utils.jl
@@ -1,7 +1,44 @@
+include("../numerics/classical_shadow.jl")
+
+
 function vec_mod(x)
     n = length(x)
     for i in 1:n
         x[i] = mod(x[i], 0:1)
     end
     return x
+end
+
+
+function measurement_Z_n(tableau)
+    b = measure_computational_basis_(tableau)
+    if isodd(count(==('1'), b))
+        return -1
+    else 
+        return 1
+    end
+end
+
+
+function Zn(n)
+    Z = [1 0; 0 -1]
+    r = 1
+    for i in 1:n
+        r = kron(r,Z)
+    end
+    return r
+end
+
+
+function FourierSeries(coefficients, period)
+    N = size(coefficients)[1]
+    m = Int((N-1)/2) 
+    function fourierSeries(t)
+        sum = zeros(Complex, size(t)[1])
+        for k in -m:m
+            sum .+= coefficients[k+m+1]*exp.(1im*2*pi/period*k.*t)
+        end
+        return real(sum)
+    end
+    return fourierSeries  
 end
